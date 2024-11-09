83c7caf68fea30c9f2b64b03ff7114346467e8ac
Rehana <teetadevil@gmail.com>
2024-11-09 15:34:29 +0530

    first commit1

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -3,7 +3,7 @@ import './possibility.css'
 import possibility_img from '../../assets/possibility_img.png'
 const Possibility = () => {
   return (
-    <div className='gpt3__possibility section__padding'>
+    <div className='gpt3__possibility section__padding' id='possibility'>
       <div className='gpt3__possibility-image'>
         <img src={possibility_img} alt="possibility" />
       </div>
